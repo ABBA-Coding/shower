f53e25c1859b5a5f28a0a6bb02b47c62de648a51
Kairat Sultan <garri.sultonov@mail.ru>
2023-10-19 01:15:13 +0600

    remove unused endpoints

--- shower/campaigns/views.py
+++ shower/campaigns/views.py
@@ -5,15 +5,6 @@ from .models import Campaign
 from .serializers import CampaignUpdateSerializer, CampaignListSerializer, CampaignCreateSerializer
 
 
-class CampaignListView(generics.ListAPIView):
-    serializer_class = CampaignListSerializer
-    permission_classes = [IsAuthenticated]
-
-    def get_queryset(self):
-        user = self.request.user
-        return Campaign.objects.filter(business__user=user).select_related("business")
-
-
 class CampaignListWithoutAuthView(generics.ListAPIView):
     queryset = Campaign.objects.all()
     serializer_class = CampaignListSerializer
