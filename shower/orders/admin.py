c95e15fe677147f564c1b68a9077caaa12aea1ee
STQT <productgsm@gmail.com>
2023-10-23 00:29:55 +0500

    orders admin custom

--- shower/orders/admin.py
+++ shower/orders/admin.py
@@ -1,11 +1,19 @@
 from django.contrib import admin
+from django.utils.timesince import timesince
+
 from .models import *
+
 
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
-    list_display = ["campaign", "amount", "currency", "status", "invoice_id"]
+    list_display = ["campaign", "amount", "currency", "status", "display_created_at", "invoice_id"]
+
+    def display_created_at(self, obj):
+        return timesince(obj.updated_at) + ' ago'
+
+    display_created_at.short_description = 'Updated at'
 
 
 @admin.register(PriceList)
 class PriceListAdmin(admin.ModelAdmin):
-    ...
+    list_display = ["amount", "currency", "is_deleted"]
